dfc8d31be0a09f88e74627b2ea6731086e80d712
LouisGonzalez <louiscrack50@gmail.com>
2019-07-29 23:57:22 -0600

    Final

--- java.js
+++ java.js
@@ -4,39 +4,32 @@ var historial2 = document.getElementById("historial2");
 var historial3 = document.getElementById("historial3");
 var historial4 = document.getElementById("historial4");
 
-function funcion(){
-    alert("funciono " + numero1.value);
-    historial.innerHTML = historial.innerHTML + numero1.value + "<br>"; 
-}   
-
-function separarCadena(){
+//METODO QUE SE ENCARGA DE SEPARAR LA CADENA DE CARACTERES UNO POR UNO 
+function separarCadena() { 
     var valor = numero1.value.length;
-    alert("la palabra es "+numero1.value);
-    
     if(/^[a-z][a-z]*/.test(numero1.value[0])==false && isNaN(numero1.value)==true && /^[!-*][!-*]*/.test(numero1.value)==false){
-        alert("La palabra inicia con un numero y tiene ademas letras");
         for(var i=0; i<numero1.value.length; i++){
             historial4.innerHTML = historial4.innerHTML + numero1.value[i]+" ";
         }
+        historial4.innerHTML = historial4.innerHTML + "<br>";
     }  
     if(isNaN(numero1.value)==false){
-        alert("la palabra solo tiene numeros");
         for(var i=0; i<numero1.value.length; i++){
             historial2.innerHTML = historial2.innerHTML + numero1.value[i]+" ";
         }
+        historial2.innerHTML = historial2.innerHTML + "<br>";
+    
     } else if (isNaN(numero1.value)==true && /^[a-z][a-z]*/.test(numero1.value[0])==true){
-        alert("La palabra es un identificador");
         for(var i=0; i<numero1.value.length; i++){     
             historial.innerHTML = historial.innerHTML + numero1.value[i]+" ";  
         }
+        historial.innerHTML = historial.innerHTML + "<br>";    
     }
     if((/^[!-*]{0,10}$/.test(numero1.value))){
-        alert("La palabra es un simbolo");   
         for(var i=0; i<numero1.value.length; i++){
             historial3.innerHTML = historial3.innerHTML + numero1.value[i]+" ";
-        }  
+        }
+        historial3.innerHTML = historial3.innerHTML + "<br>";   
     }
-   
-    historial.innerHTML = historial.innerHTML + "<br>";
     
 }
